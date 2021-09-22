508ab66654831c49683b16a3ad7ea08952c0f34a
turkey126 <isamu.takimoto.136@gmail.com>
2021-09-22 20:18:19 +0900

    connection db

--- stockapi/settings.py
+++ stockapi/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&g&f6p8pt(o#wv%3ri+nr5m73)9$8jr@mj)hgw!a2_%-b__iv'
+SECRET_KEY = 'django-insecure-_ag^f!zw*pe5=n%n*$grc+921mfwa&d)!vid_t7um5#i%dc+73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'stockapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'postgres',
+      'USER': 'postgres',
+      'HOST': 'stockdb',
+      'PASSWORD': 'postgres',
+      'PORT': 5432,
+  }
 }
 
 
